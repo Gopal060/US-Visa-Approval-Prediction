9db4ea1310247ec2de18d6a41018eb85642e28b8
Gopal060 <gopalrkate@gmail.com>
2024-09-29 20:35:26 +0530

    Data Ingestion Process Added

--- demo.py
+++ demo.py
@@ -1,12 +1,5 @@
-from us_visa.logger import logging
-from us_visa.exception import USVisaException
-import sys
+from us_visa.pipeline.training_pipeline import TrainPipeline
 
 
-logging.info("Working with custom exceptions")
-
-try:
-    2/0
-except Exception as e:
-    raise USVisaException(e, sys)
-
+obj = TrainPipeline()
+obj.run_pipeline()
